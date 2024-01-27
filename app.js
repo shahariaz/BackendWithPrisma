d8649e9c611f8fdfd49bc167615c90aabd7307a0
shahariaz <shahariaz.info@gmail.com>
2024-01-27 22:37:01 +0600

    db connected

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import express from "express";
-import dotenv from "dotenv";
-dotenv.config();
+import "dotenv/config";
 
 const app = express();
 const port = process.env.PORT || 8000;
---
+++ controllers/authController.js
@@ -0,0 +1,8 @@
+import prisma from "../src/db/db.config.js";
+
+class AuthController{
+    static async register(req,res){
+        const body = req.body;
+        
+    }
+}
