667f633065249dc502fa1538be9082c6719df5ca
shahariaz <shahariaz.info@gmail.com>
2024-01-27 20:09:12 +0600

    project setup

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+
+app.get('/', (req, res) =>{
+    res.send('Hello World');
+})
+
+
+app.listen(port, () => console.log(`server listening on ${port}`));
