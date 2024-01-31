cd5114cfa2c72aba777e94520a733d90ae0066f9
shahariaz <shahariaz.info@gmail.com>
2024-02-01 05:10:41 +0600

    news post endpoint added

--- controllers/NewsController.js
+++ controllers/NewsController.js
@@ -1,7 +1,7 @@
 import vine,{errors} from "@vinejs/vine";
 import prisma from '../DB/db.config.js';
 import { newsSchema } from "../validations/newsValidation.js";
-
+import { imageValidator,generateRandomNum } from '../utils/helper.js';
 class NewsController {
     static async index(req,res){
     }
@@ -11,7 +11,37 @@ class NewsController {
         const body = req.body;
         const validator = vine.compile(newsSchema);
         const payload =await validator.validate(body);
-        return res.status(200).json(payload);
+      if(!req.files || Object.keys(req.files).length === 0){
+        return res.status(400).json({status:400,message:"News image is required"}) 
+      }
+     
+        const image = req.files?.image;
+        //*image custom validator
+        const message = imageValidator(image?.size,image?.mimetype);
+        if(message !== null){
+          return res.status(400).json({errors:{image:message}})
+        }
+        //* image upload custom
+        const imgExt = image?.name.split(".");
+        const imageName = generateRandomNum() + "." + imgExt[1];
+        const uploadPath = process.cwd()+"/public/images"+imageName;
+        image.mv(uploadPath,(err)=>{
+          if(err) throw err;
+        
+        })
+        //* store news in database
+        payload.image = imageName;
+        payload.user_id = user.id;
+        const news = await prisma.news.create({
+          data:payload
+        })
+
+        return res.status(200).json({
+          status:200,
+          message:"News created successfully",
+          data:news
+        
+        });
         } catch (error) {
             console.log(`Error: ${error.message}`);
             if(error instanceof errors.E_VALIDATION_ERROR){
